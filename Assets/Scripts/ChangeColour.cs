e3765c8ef7d8f410ab6c126cfeaebbd7bd9b5793
JulianYao1 <115187550+JulianYao1@users.noreply.github.com>
2024-02-01 11:54:35 -0500

    added scale and rotate

---
+++ Assets/Scripts/ChangeColour.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.RuleTile.TilingRuleOutput;
+
+public class ChangeColour : ICommand
+{
+    public void Execute()
+    {
+        
+    }
+
+    public void Undo()
+    {
+        
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -27,19 +27,34 @@ public class Player : MonoBehaviour
             gm.AddCommand(c);
         }
 
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            ICommand c = new MoveDown(transform);
+            gm.AddCommand(c);
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            ICommand c = new MoveUp(transform);
+            gm.AddCommand(c);
+        }
+
         if (Input.GetKeyDown(KeyCode.Q))
         {
             ComboCommand c = new ComboCommand(this);
             c.AddStep(new MoveRight(transform));
             c.AddStep(new MoveRight(transform));
             c.AddStep(new MoveLeft(transform));
+            c.AddStep(new Rotate(transform));
             c.AddStep(new MoveLeft(transform));           
             c.AddStep(new MoveRight(transform));
+            c.AddStep(new ScaleDown(transform));
             c.AddStep(new MoveRight(transform));
             c.AddStep(new MoveLeft(transform));
             c.AddStep(new MoveLeft(transform));
             c.AddStep(new MoveLeft(transform));
             c.AddStep(new MoveLeft(transform));
+            c.AddStep(new ScaleUp(transform));
             gm.AddCommand(c);
         }
 
